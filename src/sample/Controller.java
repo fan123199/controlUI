a72dc85fbccf354c98684cf90c2f32ad39895aed
fan123199 <fan123199@gmail.com>
2016-09-19 23:49:14 +0800

    try controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Controller {
+    Logger logger = Logger.getLogger("hehe");
+
+    @FXML private Button tv_volume_down;
+
+    @FXML
+    protected void onHan(ActionEvent actionEvent) {
+            logger.log(Level.ALL, "button use");
+
+    }
 }
